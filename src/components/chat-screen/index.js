7915e5961454684ad5d6c638b98a3ba2894e604e
Valera Vilks <valeravilks@gmail.com>
2019-09-12 14:29:43 +0300

    Complete views chat screen

--- src/components/chat-screen/index.js
+++ src/components/chat-screen/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import withStore from '~/hocs/withStore';
+import { Form, Button, Card } from 'react-bootstrap';
 
 class First extends React.Component{
     render(){
         return (
             <div>
-                First
+                <h1 className="text-center mb-5 mt-5">Lonely Chat</h1>
+                <Card border="light">
+                    <Card.Header>Валерий</Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk
+                            of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Form className="text-center">
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Control as="textarea" rows="3" placeholder="Enter your message"/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Send Message
+                    </Button>
+                </Form>
             </div>
         );
     }
