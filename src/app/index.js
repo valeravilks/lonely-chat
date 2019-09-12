feb2a25aec6f144794d1494fa8b3d0ed32a20a4a
Valera Vilks <valeravilks@gmail.com>
2019-09-12 14:55:46 +0300

    Check name in sessionStorage

--- src/app/index.js
+++ src/app/index.js
@@ -5,13 +5,21 @@ import React from 'react';
 import withStore from '~/hocs/withStore';
 
 class App extends React.Component{
+
     render(){
+        let screen;
+
+        if(this.props.stores.storage.nameCheck()) {
+            screen = <FirstScreen />
+        } else {
+            screen = <ChatScreen />
+        }
+
         return(
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-12 col-lg-6">
-                        <FirstScreen/>
-                        <ChatScreen/>
+                        {screen}
                     </div>
                 </div>
             </div>
--- src/store/localStorageStore.js
+++ src/store/localStorageStore.js
@@ -1,6 +1,13 @@
 import {observable, computed, action} from 'mobx';
 
 export default class{
+    @action nameCheck = () => {
+        let name = sessionStorage.getItem('name');
 
-
+        if(name){
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
